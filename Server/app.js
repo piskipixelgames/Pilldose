5bf031d57a7f23791c4ffee7280c4083df3035d3
Wajahat Hafeez <wajahathafeez62@gmail.com>
2026-06-01 11:35:33 +0500

    public public update

--- Server/app.js
+++ Server/app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"));
