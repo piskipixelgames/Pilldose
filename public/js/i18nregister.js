c5fad36a61db52cdc10ac8d9c1128f412c09fc9e
Wajahat Hafeez <wajahathafeez62@gmail.com>
2026-06-15 16:21:09 +0500

    commit register changes

---
+++ public/js/i18nregister.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  document.title = t.pageTitle;
+
+  document.getElementById("beginJourney").textContent = t.beginJourney;
+
+  document.getElementById("heading").textContent = t.heading;
+
+  document.getElementById("description").textContent = t.description;
+
+  document.getElementById("hipaaTitle").textContent = t.hipaaTitle;
+
+  document.getElementById("hipaaDesc").textContent = t.hipaaDesc;
+
+  document.getElementById("insightsTitle").textContent = t.insightsTitle;
+
+  document.getElementById("insightsDesc").textContent = t.insightsDesc;
+
+  document.getElementById("personalInfo").textContent = t.personalInfo;
+
+  document.getElementById("fullNameLabel").textContent = t.fullName;
+
+  document.getElementById("emailAddressLabel").textContent = t.emailAddress;
+
+  document.getElementById("profileType").textContent = t.profileType;
+
+  document.getElementById("patientTitle").textContent = t.patient;
+
+  document.getElementById("patientDesc").textContent = t.patientDesc;
+
+  document.getElementById("caregiverTitle").textContent = t.caregiver;
+
+  document.getElementById("caregiverDesc").textContent = t.caregiverDesc;
+
+  document.getElementById("accountSecurity").textContent = t.accountSecurity;
+
+  document.getElementById("createPassword").textContent = t.createPassword;
+
+  document.getElementById("rule-length-text").textContent = t.ruleLength;
+
+  document.getElementById("rule-number-text").textContent = t.ruleNumber;
+
+  document.getElementById("rule-symbol-text").textContent = t.ruleSymbol;
+
+  document.getElementById("completeSignupBtn").textContent =
+
+    t.completeSignup;
+
+  document.getElementById("alreadyAccount").textContent =
+
+    t.alreadyAccount;
+
+  document.getElementById("loginLink").textContent =
+
+    t.login;
+
+  document.getElementById("privacyPolicyLink").textContent =
+
+    t.privacyPolicy;
+
+  document.getElementById("termsLink").textContent =
+
+    t.terms;
+
+  document.getElementById("hipaaComplianceLink").textContent =
+
+    t.hipaaCompliance;
+
+  document.getElementById("accessibilityLink").textContent =
+
+    t.accessibility;
+
+  document.getElementById("footerText").textContent =
+
+    t.footer;
+
+});
