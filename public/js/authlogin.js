a0cf8fdce892507425975e9d04bf26f291dc9a89
Wajahat Hafeez <wajahathafeez62@gmail.com>
2026-07-01 00:07:38 +0500

    Toggle button fix

--- public/js/authlogin.js
+++ public/js/authlogin.js
@@ -30,6 +30,26 @@ console.log(toast.className);
   }, 3000);
 }
 
+function toggleLanguage() {
+
+    const current =
+        localStorage.getItem("language") || "en";
+
+    const next =
+        current === "en"
+            ? "es"
+            : "en";
+
+    localStorage.setItem(
+        "language",
+        next
+    );
+
+    window.APP_CONFIG.LANGUAGE =
+        next;
+
+    applyTranslations();
+}
 
 
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
